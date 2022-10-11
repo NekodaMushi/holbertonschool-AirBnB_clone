42c1d06075f70a903af0cbb3be50b957d8d4734e
NekodaMushi <4252@holbertonschool.com>
2022-10-11 07:36:39 +0000

    Testing code from Toulouse

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from models.engine.file_storage import FileStorage
+from models.engine import file_storage
 
-storage = FileStorage()
-storage.reload()
+storage = file_storage.FileStorage()
+file_storage.FileStorage.reload(storage)
