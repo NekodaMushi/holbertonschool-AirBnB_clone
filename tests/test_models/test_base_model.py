3c2236478fb1fa36b49665efe5d7fe7bc73f11f3
NekodaMushi <4252@holbertonschool.com>
2022-10-11 08:10:00 +0000

    str

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,15 +2,21 @@
 """Unittest"""
 import unittest
 from datetime import datetime
+from unittest.mock import Base
 from models.base_model import BaseModel
 
 
 class BaseModelTest(unittest.TestCase):
     """BaseModel Test Class"""
-    def testSave(self):
+    def testId(self):
         b0= BaseModel()
         b0.number = 32
         b0.string = "Chung"
         b0.fail = None
         b0.save()
         self.assertNotEqual(b0.created_at, b0.updated_at)
+
+    def test__str__(self):
+        b1 = BaseModel()
+        checker = [f"[{(b1).__name__}] ({b1.id}) {b1.__dict__}"]
+        self.assertEqual(b1, checker)
