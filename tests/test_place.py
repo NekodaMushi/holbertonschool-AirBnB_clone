d15d8d4e36ff6d820c887002cf8a6dbb70f86a84
NekodaMushi <4252@holbertonschool.com>
2022-10-12 15:43:51 +0000

    Test Task 8

---
+++ tests/test_place.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Unittest Place"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """Place Test Class"""
+
+    def test_var_validity(self):
+        """Method testing validity 
+        of variable in Place"""
+        p = Place()
+        self.assertTrue(p, "city_id")
+        self.assertTrue(p, "user_id")
+        self.assertTrue(p, "name")
+        self.assertTrue(p, "description")
+        self.assertTrue(p, "number_rooms")
+        self.assertTrue(p, "number_bathrooms")
+        self.assertTrue(p, "max_guest")
+        self.assertTrue(p, "price_by_night")
+        self.assertTrue(p, "latitude")
+        self.assertTrue(p, "longitude")
+        self.assertTrue(p, "amenity")
+        self.assertEqual(p.city_id, "")
+        self.assertEqual(p.user_id, "")
+        self.assertEqual(p.name, "")
+        self.assertEqual(p.description, "")
+        self.assertEqual(p.number_rooms, "")
+        self.assertEqual(p.number_bathrooms, "")
+        self.assertEqual(p.max_guest, "")
+        self.assertEqual(p.price_by_night, "")
+        self.assertEqual(p.latitude, "")
+        self.assertEqual(p.amenity_ids, "")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_review.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Review Test"""
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """Review Test Class"""
+
+
+    def test_var_validity(self):
+        """Method testing variables
+        validity in class Review"""
+        r = Review()
+        self.assertTrue(hasattr(r, "place_id" ))
+        self.assertTrue(hasattr(r, "user_id"))
+        self.assertTrue(hasattr(r, "test"))
+        self.assertEqual(r.place_id, "")
+        self.assertEqual(r.user_id, "")
+        self.assertEqual(r.text, "")
